da4221499253b76dd481e54511c2de1753bd2aec
toiHocCode <96515211+toiHocCode@users.noreply.github.com>
2022-07-05 15:59:53 +0700

    Create KieuCanBo.java

---
+++ src/models/KieuCanBo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Admin
+ */
+public enum KieuCanBo {
+    GiaoVien, CanBoTapVu, CanBoNghienCuu,None
+}
